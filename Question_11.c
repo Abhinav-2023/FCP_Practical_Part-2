0c648c2ac0ded114f4271076577793a72c820d43
Abhinav-2023 <abhinavsingh@ece.du.ac.in>
2023-12-24 15:33:37 +0530

    commit

--- Question_11.c
+++ Question_11.c
@@ -5,5 +5,13 @@ void main()
     int arr[] = {1,2,3,4,5};
     int *ptr = arr;
 
-    printf("a. Increment a pointer: %d\n", *(ptr++))
+    printf("a. Increment a pointer: %d\n", *(++ptr));
+
+    printf("b. Decrement a pointer: %d\n", *(--ptr));
+
+    printf("c. Add an integer to a pointer: %d\n", *(ptr + 2));
+
+    printf("d. Subtract an integer from a pointer: %d\n", *(ptr - 4));
+
+    printf("e. Subtract two pointers: %d\n", ptr - arr);
 }
